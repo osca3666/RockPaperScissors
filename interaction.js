794b0671fe3bb59680ff497f4f5988f39aa20193
githubUsername <emailLinkedtoGithubAcct@email.com>
2023-04-03 21:15:24 +0000

    yes

--- interaction.js
+++ interaction.js
@@ -1,8 +1,17 @@
-// const button_r = document.querySelector('button.rock');
-// const button_s = document.querySelector('button.scizzor')
-// const button_p = document.querySelector('button.paper');
+const button_r = document.querySelector('button.rock');
+const button_p = document.querySelector('button.paper');
+const button_s = document.querySelector('button.scizzor')
 
-// const button = document.querySelector('button');
+button_r.onlick= playRound('ROCK', getComputerChoice());
+button_p.onlick= playRound('PAPER', getComputerChoice());
+button_s.onlick= playRound('SCIZZOR', getComputerChoice());
+
+const div = document.createElement('div');
+div.color = red;
+// body.appendChild(div);
+// div.borderStyle = 1px solid red;
+
+const button = document.querySelector('button');
 
 function getRandomNumber(min, max){
     min = Math.ceil(min);
@@ -14,7 +23,6 @@ function getComputerChoice() {
     let diceRollResult = getRandomNumber(1,3);
     return diceRollResult;
 }
-
 
 function playRound(playerChoice, computerChoice)
 {
@@ -81,27 +89,27 @@ function playRound(playerChoice, computerChoice)
 
 
 
-function game()
-{
-    let playerWins = 0;
-    let computerWins = 0;
+// function game()
+// {
+//     let playerWins = 0;
+//     let computerWins = 0;
 
-    for(i = 0; i < 100; i++)
-    {
-        let playerChoice = prompt("rock,paper,or scizzor boiii?");
-        playerChoice = playerChoice.toUpperCase();
-        let computerChoice = getComputerChoice(); //returns 1 2 or 3
-        result = playRound(playerChoice,computerChoice);
-        if (result == "win")
-        {
-            playerWins++;
-        }
-        else if ( result == "lost")
-        {
-            computerWins++;
-        }
-        console.log("player wins: " + playerWins + "computer wins: " + computerWins);
-    }
-}
+//     for(i = 0; i < 5; i++)
+//     {
+//         let playerChoice = prompt("rock,paper,or scizzor boiii?");
+//         playerChoice = playerChoice.toUpperCase();
+//         let computerChoice = getComputerChoice(); //returns 1 2 or 3
+//         result = playRound(playerChoice,computerChoice);
+//         if (result == "win")
+//         {
+//             playerWins++;
+//         }
+//         else if ( result == "lost")
+//         {
+//             computerWins++;
+//         }
+//         console.log("player wins: " + playerWins + "computer wins: " + computerWins);
+//     }
+// }
 
 game();
